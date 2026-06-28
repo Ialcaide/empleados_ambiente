fae8527e10625187daa5dcb0fe63410b85fe6f91
Ialcaide <ialcaidel@unemi.edu.ec>
2026-06-27 23:26:20 -0500

    Fix navbar y redirect pagina principal

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('empleado_lista'), name='home'),
     path('', include('autenticacion.urls')),
     path('', include('empleados.urls')),
     path('', include('empleados_vbc.urls')),
